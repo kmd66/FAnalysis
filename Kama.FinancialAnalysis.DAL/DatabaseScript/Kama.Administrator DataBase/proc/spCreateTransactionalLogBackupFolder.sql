eba80173734d06544d89084ef225d2ab48a9a5cc
kamatech-ir <98743650+kamatech-ir@users.noreply.github.com>
2023-09-08 10:02:41 +0330

    .

--- Kama.FinancialAnalysis.DAL/DatabaseScript/Kama.Administrator DataBase/proc/spCreateTransactionalLogBackupFolder.sql
+++
@@ -1,8 +0,0 @@
-USE [Kama.Administrator]
-GO
-CREATE PROC [dsk].[spCreateTransactionalLogBackupFolder]
-AS
-     DECLARE @dirPath VARCHAR(1024), @fileDate VARCHAR(20)= CONVERT(VARCHAR(20), GETDATE(), 112);
-     SET @dirPath = '\\arodg01.arodc.ir\DG01\LogBackup\' + @fileDate + '\';
-     EXEC xp_create_subdir 
-          @dirPath;
--- Kama.FinancialAnalysis.DAL/DatabaseScript/Kama.Administrator DataBase/proc/spDeleteFullBackupFolder.sql
+++
@@ -1,8 +0,0 @@
-USE [Kama.Administrator]
-GO
-CREATE OR ALTER PROC [dsk].[spDeleteFullBackupFolder]
-AS
-     DECLARE @DeletedirPath VARCHAR(1024), @DeletefileDate VARCHAR(20)= CONVERT(VARCHAR(20), DATEADD(DAY, -7, GETDATE()), 112);
-     SET @DeletedirPath = '\\arodg01.arodc.ir\DG01\Full\' + @DeletefileDate;
-     EXEC master.sys.xp_delete_files 
-          @DeletedirPath;
